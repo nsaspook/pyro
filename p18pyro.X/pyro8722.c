a1a70a80150b2d24e34d697923a9825099d4d300
Fred Brooks <fred@sma2.rain.com>
2016-08-15 09:56:14 -0700

    update

--- p18pyro.X/pyro8722.c
+++ p18pyro.X/pyro8722.c
@@ -569,12 +569,12 @@ void main(void) // Lets Party
 					voltfp(L.adc_val[GAS_MFC], f1); // mfc 1 readback
 					voltfp(L.adc_val[AIR_MFC_SP], f2); // mfc 0 setpoint
 					voltfp(L.adc_val[GAS_MFC_SP], f3); // mfc 1 setpoint
-					sprintf(bootstr2, "%s %s %s %sV                       ", f0, f2, f1, f3);
+					sprintf(bootstr2, "%s %s %s %sV                      ", f0, f2, f1, f3);
 					DLED_4 = HIGH;
 					lcd_display_line(bootstr2, LL2);
 					sprintf(bootstr2, "%u %u %d                          ", L.adc_raw[AIR_MFC], L.adc_raw[AIR_MFC_SP], (int16_t) (L.adc_raw[AIR_MFC] - L.adc_raw[AIR_MFC_SP]));
 					lcd_display_line(bootstr2, LL3);
-					sprintf(bootstr2, "%lu %lu SLM : %u %u %u                     ", mfc[AIR_MFC].mfc_integ_current_mass / MFC_INTEG,
+					sprintf(bootstr2, "%lu %lu SLM : %u %u %u            ", mfc[AIR_MFC].mfc_integ_current_mass / MFC_INTEG,
 						(uint32_t) ((float) (mfc[AIR_MFC].mfc_integ_current_mass / MFC_INTEG) * mfc[AIR_MFC].scale_out),
 						V.t4_prev, V.t4_now, PORTGbits.RG0);
 					lcd_display_line(bootstr2, LL4);
